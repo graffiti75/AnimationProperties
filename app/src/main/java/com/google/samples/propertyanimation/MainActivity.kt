75022d289be70cf9be7a69e2c7c84a0ca262dc32
Rodrigo Cericatto Konzen <graffiti75@gmail.com>
2023-02-27 17:30:28 -0500

    Adding code from section "4.6.7. (4:02) Colorizing"

--- app/src/main/java/com/google/samples/propertyanimation/MainActivity.kt
+++ app/src/main/java/com/google/samples/propertyanimation/MainActivity.kt
@@ -4,6 +4,7 @@ import android.animation.Animator
 import android.animation.AnimatorListenerAdapter
 import android.animation.ObjectAnimator
 import android.animation.PropertyValuesHolder
+import android.graphics.Color
 import android.os.Bundle
 import android.view.View
 import android.widget.Button
@@ -102,6 +103,15 @@ class MainActivity : AppCompatActivity() {
 	}
 
 	private fun colorizer() {
+//		var animator = ObjectAnimator.ofInt(star.parent,
+//			"backgroundColor", Color.BLACK, Color.RED)
+		var animator = ObjectAnimator.ofArgb(star.parent,
+			"backgroundColor", Color.BLACK, Color.RED)
+		animator.setDuration(500)
+		animator.repeatCount = 1
+		animator.repeatMode = ObjectAnimator.REVERSE
+		animator.disableViewDuringAnimation(colorizeButton)
+		animator.start()
 	}
 
 	private fun shower() {
